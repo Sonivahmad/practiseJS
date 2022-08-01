1fcad58c0fe504efc2d672a4f974d7168879276e
soniv ahmad <sonivahmad@gmail.com>
2022-08-01 21:52:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let arr=[10,2,3,4,1,7,8];
+let even=[];
+for(let i=0;i<arr.length;i++){
+    if(arr[i]%2==0){
+   }else{
+    console.log("odd");
+   }
+}
+console.log(even);
+
+
+
+
+
---
+++ index2.js
@@ -0,0 +1,8 @@
+let obj={
+    name:"vasanth",
+    age:20,
+    city:"chennai"
+};
+for(let key in obj){
+    console.log(key, obj[key]);
+}
